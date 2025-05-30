b2da69c9e4fe8a024ed92f7e7d4f3914e6b7e930
CanlasJustin <dev.justinredcanlas@gmail.com>
2025-05-30 20:54:48 +0800

    added routing

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { authGuard } from './services/auth.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'weather', component: WeatherComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' },
+];
